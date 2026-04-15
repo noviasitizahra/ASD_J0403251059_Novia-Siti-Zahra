47598f85fa9a9c1d0ef6963824e19b933d2a2f61
noviasitizahra <noviasitizahra2006>
2026-04-15 15:38:33 +0700

    preorder

--- Pertemuan_9/materi2.py
+++ Pertemuan_9/materi2.py
@@ -35,35 +35,7 @@ class Node:
 # penjelasan 
 
 
-# ============================================
-# Nama  : Novia Siti Zahra
-# Kelas : B1
-# NIM   : J0403251059
-# Latihan 3 : membuat binary search
-# ============================================
-class Node:
-    def __init__(self, data):
-        self.data = data
-        self.left = None
-        self.right = None
 
-    def preorder(node):
-        if node is not 
-
-# membuat root
-        root = Node("A")
-
-        # membuat child level 1
-        root.left = Node("B")
-        root.right = Node("C")
-
-        # membuat child level 2
-        root.left.left = Node("D")
-        root.left.right = Node("E")
-
-        # menjalankan traversal preorder
-        print("Hasil traversal preorder: ")
-        preorder(root)
 
 # ============================================
 # Nama  : Novia Siti Zahra
---
+++ Pertemuan_9/materi3.py
@@ -0,0 +1,30 @@
+# ============================================
+# Nama  : Novia Siti Zahra
+# Kelas : B1
+# NIM   : J0403251059
+# Latihan 3 : membuat binary search
+# ============================================
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+    def preorder(node):
+        if node is not None:
+
+
+        # membuat root
+        root = Node("A")
+
+        # membuat child level 1
+        root.left = Node("B")
+        root.right = Node("C")
+
+        # membuat child level 2
+        root.left.left = Node("D")
+        root.left.right = Node("E")
+
+        # menjalankan traversal preorder
+        print("Hasil traversal preorder: ")
+        preorder(root)

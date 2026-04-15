3bfa35d27a30ab8105e69a4c31669364c69300a0
noviasitizahra <noviasitizahra2006>
2026-04-15 16:01:53 +0700

    4

--- Pertemuan_9/materi2.py
+++ Pertemuan_9/materi2.py
@@ -5,7 +5,6 @@
 # Latihan 2 : membuat binary search
 # ============================================
 
-
 # class node digunakan untuk dasar dari tree
 
 class Node:
@@ -35,37 +34,6 @@ class Node:
 # penjelasan 
 
 
-# ============================================
-# Nama  : Novia Siti Zahra
-# Kelas : B1
-# NIM   : J0403251059
-# Latihan 4 : membuat traversal inorder
-# ============================================
-
-class Node:
-    def __init__(self, data):
-        self.data = data # menyimpan nilai node
-        self.left = None # child kiri
-        self.right = None # child kanan
-
-# membuat fungsi inorder: left -> root -> right
-def inorder(node):
-    if node is not None:
-        inorder(node.left)
-        inorder(node.data, end="")
-        inorder(node.right)
-
-    # membuat tree
-    # membuat root
-        root = Node("A")
-
-        # membuat child level 1
-        root.left = Node("B")
-        root.right = Node("C")
-
-        # membuat child level 2
-        root.left.left = Node("D")
-        root.left.right = Node("E")
 
 # ============================================
 # Nama  : Novia Siti Zahra

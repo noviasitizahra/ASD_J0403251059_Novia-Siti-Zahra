6cf5bc1c91e538aca83986fed56aa3bea2ed8b64
noviasitizahra <noviasitizahra2006>
2026-04-29 20:20:15 +0700

    jawaban lat 2

--- Pertemuan_11/praktikum11-latihan2.py
+++ Pertemuan_11/praktikum11-latihan2.py
@@ -8,7 +8,8 @@
 # Latihan 2 : Studi Kasus DFS (Eksplorasi Jalur) 
 # ====================================================
 
-# Graph berikut merepresentasikan jalur eksplorasi: 
+# Graph berikut merepresentasikan jalur eksplorasi dalam bentuk dictionary: 
+# Key = node, Value = daftar node tetangga yang terhubung
 graph = { 
     'A': ['B', 'C'], 
     'B': ['D', 'E'], 
@@ -19,16 +20,20 @@ graph = {
 } 
 
 # Gunakan algoritma DFS untuk menelusuri graph mulai dari node A. 
+# Fungsi DFS untuk menelusuri graph
 def dfs(graph, node, visited): 
-    visited.add(node) 
-    print(node, end=" ") 
+    visited.add(node)       # Tandai node sebagai sudah dikunjungi
+    print(node, end=" ")    # Cetak node yang sedang dikunjungi
 
+    # Telusuri semua tetangga dari node
     for neighbor in graph[node]: 
-        if neighbor not in visited: 
-            dfs(graph, neighbor, visited)
+        if neighbor not in visited:         # Jika tetangga belum dikunjungi
+            dfs(graph, neighbor, visited)   # Rekursif masuk ke tetangga
 
+# Set untuk menyimpan node yang sudah dikunjungi
 visited = set() 
 
+# Panggil fungsi DFS mulai dari node 'A'    
 print("DFS dari A:") 
 dfs(graph, 'A', visited) 
 
@@ -38,5 +43,14 @@ Pertanyaan Analisis
 2. Apa yang terjadi jika urutan neighbor diubah?  
 3. Bandingkan hasil DFS dengan BFS pada graph yang sama. 
 
+Jawaban:
+1. Karena DFS menggunakan rekursi (atau stack) yang selalu masuk ke cabang berikutnya sampai habis,
+    baru kembali ke atas. Jadi jalur terdalam dieksplorasi dulu.
 
+2. Urutan kunjungan node juga ikut berubah. DFS akan mengikuti urutan tetangga yang ada di list graph.
+      Misalnya kalau 'A' punya ['C','B'] bukan ['B','C'], maka DFS akan masuk ke 'C' dulu.
+
+3. DFS: fokus ke jalur dalam dulu (contoh hasil: A B D E C F).
+    BFS: fokus ke level demi level (contoh hasil: A B C D E F).
+    Jadi urutan kunjungan berbeda karena cara kerja algoritmanya berbeda.
 '''

73c1e5c33f29653f9358efe2a578bc6a976a5e74
noviasitizahra <noviasitizahra2006>
2026-05-06 15:18:07 +0700

    p12 materi 2

---
+++ Pertemuan_12/praktikum12-materi2.py
@@ -0,0 +1,27 @@
+# ==========================================
+# Nama  : Novia Siti Zahra
+# NIM   : J0403251059
+# Kelas : B1
+# ==========================================
+
+# ==========================================
+# materi 2 : Algoritma Bellman Ford 
+# ==========================================
+
+def bellman_ford(graph, start): 
+ 
+    distances = {node: float('inf') for node in graph} 
+    distances[start] = 0 
+ 
+    # Relaksasi berulang 
+    for _ in range(len(graph) - 1): 
+ 
+        for node in graph: 
+ 
+            for neighbor, weight in graph[node].items(): 
+ 
+                if distances[node] + weight < distances[neighbor]: 
+ 
+                    distances[neighbor] = distances[node] + weight 
+ 
+    return distances
